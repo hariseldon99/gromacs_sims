bcd957c9808de15e6f61fe543bf70b8af8eaa496
Analabha Roy <daneel@utexas.edu>
2024-09-16 13:28:37 +0530

    reran complex simulation upto energy minimization locally with index groups and checkpointed

--- zeb_hb/zeb_hb_phz_complex_equilibrate.py
+++ zeb_hb/zeb_hb_phz_complex_equilibrate.py
@@ -17,6 +17,7 @@ DATA_OUT = 'zeb_hb_phz_complex_sim'
 sys_top_folder = os.path.join(DATA_OUT, 'sys_top')
 # Equillibration
 equi_folder = os.path.join(DATA_OUT, 'sys_equi')
+vsite='none'
 HA_time = 0.5
 CA_time = 1.0
 CA_LOW_time = 4.0
@@ -29,7 +30,7 @@ CA_step = 1000 * CA_time / dt
 CA_LOW_step = 1000 * CA_LOW_time / dt
 
 #Load from checkpoint
-with open('checkpoint_em_20240915.pycpt', 'rb') as py_cpt:
+with open('checkpoint_em_20240916.pycpt', 'rb') as py_cpt:
     complex_sys = pickle.load(py_cpt)
 
 #Parallelization
@@ -46,11 +47,11 @@ complex_sys.em_equi_three_step_iter_error(out_folder=equi_folder,
     nsteps_CA=CA_step,
     nsteps_CA_LOW=CA_LOW_step,
     dt=dt, dt_HA=dt_HA,
-    tc_grps='Protein HZ1 Water_and_ions',
-    tau_t= '0.1 0.1 0.1',
-    ref_t= '310 310 310',
+    tc_grps='Protein_PHZ Water_and_ions',
+    tau_t= '0.1 0.1',
+    ref_t= '310 310',
     vsite=vsite, maxwarn=10, iter_num=1)
 
 #Checkpoint again
-with open('checkpoint_equi_20240915.pycpt', 'wb') as py_cpt:
+with open('checkpoint_equi_20240916.pycpt', 'wb') as py_cpt:
     pickle.dump(complex_sys, py_cpt)
