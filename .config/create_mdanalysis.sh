d21082f2d6c78b9ebecd0f5fb52ce9977b908a47
hariseldon99 <hariseldon99@gmail.com>
2024-12-20 23:13:23 +0530

    removed unnecessary sifs for param or local. mdanalysis is now uptodate on conda-forge

--- .config/create_mdanalysis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-conda create -n mdanalysis-develop numpy cython
-conda activate mdanalysis-develop
-git clone --depth=50 https://github.com/MDAnalysis/mdanalysis.git
-cd mdanalysis
-pip install package/
