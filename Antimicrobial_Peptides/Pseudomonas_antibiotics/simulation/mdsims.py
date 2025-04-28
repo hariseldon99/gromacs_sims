ffaffa13cf267b1535676301eaf8909b57a51fc0
hariseldon99 <hariseldon99@gmail.com>
2025-04-28 21:12:55 +0530

    Rahul manually reprotonated the ligands post-docking. Noe getting ready for batch mdsim

--- Antimicrobial_Peptides/Pseudomonas_antibiotics/simulation/mdsims.py
+++ Antimicrobial_Peptides/Pseudomonas_antibiotics/simulation/mdsims.py
@@ -15,11 +15,13 @@ def chunk_into_n(lst, n):
     list(range(n)))
   )
 
-complexes_dir = "./simulation_complexes"
+complexes_dir = "./simulation_complexes_protonationfixed"
 
 complex_pdbs = [
-     "pbppen1999.pdb", "aminostep2016.pdb", "levgyra2018.pdb"
-    ,"pbpmez2018.pdb", "pbppen2018.pdb"
+	"aminostep2012C.pdb",  "levparc2018C_chainB.pdb",  "ndxparc2018C_chainA.pdb",
+	"aminostep2016C.pdb",  "ndxgyra1996C.pdb",         "pbpmez2018C.pdb",
+	"aminotob2016C.pdb",   "ndxgyra2018C.pdb",         "pbppen1999C.pdb",
+	"levgyra2018C.pdb",    "ndxparc1999C_chainB.pdb",  "pbppen2018C.pdb"
 ]
 
 complexes = []
