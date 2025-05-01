293342ac5fe8ddb63bd3727be0ec9dbaf612622d
hariseldon99 <hariseldon99@gmail.com>
2025-05-01 21:09:08 +0530

    added python scripts for batch docking

---
+++ AYUSH_Diabetes_PCOS/batch_docking_mpi.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+import importmonkey 
+import argparse
+parser = argparse.ArgumentParser(description="Batch docking script for receptors and ligands.")
+parser.add_argument("--bd_modpath", type=str, required=True, 
+                    help="Directory containing blind docking module")
+parser.add_argument("--receptors_dir", type=str, required=True, 
+                    help="Directory containing receptor PDB files.")
+parser.add_argument("--ligands_dir", type=str, required=True, 
+                    help="Directory containing ligand PDB files.")
+parser.add_argument("--protonate_receptors", action="store_true", 
+                    help="Flag to protonate receptors.")
+parser.add_argument("--protonate_ligands", action="store_true", 
+                    help="Flag to protonate ligands.")
+
+args = parser.parse_args()
+
+receptors_dir = args.receptors_dir
+ligands_dir = args.ligands_dir
+protonate_receptors = args.protonate_receptors
+protonate_ligands = args.protonate_ligands
+importmonkey.add_path(args.bd_modpath)  
+
+import blind_docking as bd
+from multiprocessing import cpu_count
+import os
+nprocs_loc = os.environ.get("OMP_NUM_THREADS", cpu_count())
+from mpi4py import MPI
+
+from math import ceil
+import argparse
+def chunk_into_n(lst, n):
+  size = ceil(len(lst) / n)
+  return list(
+    map(lambda x: lst[x * size:x * size + size],
+    list(range(n)))
+  )
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+if rank == 0:
+    receptors = [os.path.join(receptors_dir, f) for f in os.listdir(receptors_dir) if f.endswith('.pdb')]
+    print(f"Found {len(receptors)} receptors in {receptors_dir}.")
+
+    ligands = [os.path.join(ligands_dir, f) for f in os.listdir(ligands_dir) if f.endswith('.pdb')]
+    print(f"Found {len(ligands)} ligands in {ligands_dir}.")
+
+    print("Preparing receptors and ligands for docking...")
+    receptor_pdbqts = []
+    #prepare receptors for docking
+    for r in receptors:
+        name = os.path.basename(r).split(".")[0]
+        print(f"Preparing receptor {name} for docking...")
+        receptor_pdbqts.append(bd.prepare_receptor(r, protonate=args.protonate_receptors))
+        print(f"{name} prepared.")
+
+    #prepare ligands for docking
+    ligand_pdbqts = []
+    for ligand in ligands:
+        if ligand.endswith('.pdb'):
+            ligand_name = os.path.basename(ligand).split(".")[0]
+            print(f"Preparing {ligand_name} for docking...")
+            ligand_pdbqts.append(bd.prepare_ligand(ligand, protonate=args.protonate_ligands))
+            print(f"{ligand_name} prepared.")
+        else:
+            print(f"{ligand} is not a PDB file, skipping.")
+
+    print("All receptors and ligands prepared for docking.")
+    print("Starting batch docking...")
+else:
+    receptor_pdbqts = None
+    ligands = None
+# Broadcast the ligands list to all processes
+ligands = comm.bcast(ligands, root=0)
+
+# Scatter the receptor_pdbqts list across the communicator
+if rank == 0:
+    receptor_pdbqts_loc = chunk_into_n(receptor_pdbqts, size)
+else:
+    receptor_pdbqts_loc = None
+
+
+# Scatter the receptor_pdbqts list
+receptor_pdbqts_loc = comm.scatter(receptor_pdbqts_loc, root=0)
+
+# Each process will now have a portion of the receptor_pdbqts list
+for i, receptor in enumerate(receptor_pdbqts_loc):
+    center_x, center_y, center_z = bd.calculate_center_of_mass(receptor)
+    box_size = bd.estimate_box_size(receptor)
+    size_x, size_y, size_z = box_size
+    bd.batch_docking(receptor, ligands_dir, 
+                  center_x, center_y, center_z, 
+                  size_x, size_y, size_z,
+                  nprocs_loc)
+    print(f"Process {rank} completed docking for receptor {i+1}/{len(receptor_pdbqts_loc)}: {receptor}")
+#TODO: After docking, split the output files into separate files for each receptor-ligand pair and deprotonate the ligand files. Optionally, use openbabel to reprotonate the ligand files, as docking screws with protonation states 
---
+++ AYUSH_Diabetes_PCOS/blind_docking.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+import argparse
+from Bio.PDB import PDBParser
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from openbabel import openbabel
+
+def protonate_protein(protein_pdb):
+    """
+    Protonate the protein using OpenBabel.
+    """
+
+    obConversion = openbabel.OBConversion()
+    obConversion.SetInAndOutFormats("pdb", "pdb")
+
+    mol = openbabel.OBMol()
+    if not obConversion.ReadFile(mol, protein_pdb):
+        raise ValueError(f"Could not read protein PDB file: {protein_pdb}")
+
+    # Add hydrogens
+    mol.AddHydrogens()
+
+    protonated_pdb = protein_pdb.replace('.pdb', '_h.pdb')
+    if not obConversion.WriteFile(mol, protonated_pdb):
+        raise ValueError(f"Could not write protonated protein PDB file: {protonated_pdb}")
+
+    print(f"Protein protonated successfully: {protonated_pdb}")
+    return protonated_pdb
+
+def protonate_ligand(ligand_pdb):
+        """
+        Protonate the ligand using RDKit.
+        """
+        print(ligand_pdb)
+        ligand_mol = Chem.MolFromPDBFile(ligand_pdb, removeHs=False)
+        if ligand_mol is None:
+            raise ValueError(f"Could not parse ligand PDB file: {ligand_pdb}")
+        
+        # Add hydrogens
+        ligand_mol = Chem.AddHs(ligand_mol)
+        
+        # Perform geometry optimization
+        AllChem.EmbedMolecule(ligand_mol, AllChem.ETKDG())
+        AllChem.UFFOptimizeMolecule(ligand_mol)
+        # Get the residue name from the ligand PDB
+        with open(ligand_pdb, 'r') as f:
+            for line in f:
+                if line.startswith("HETATM") or line.startswith("ATOM"):
+                    residue_name = line[17:20].strip()
+                    print(f"Residue name found: {residue_name}")
+                    break
+                else:
+                    raise ValueError("Could not determine residue name from ligand PDB file.")
+
+        
+        # Write the protonated ligand back to PDB format
+        protonated_pdb = ligand_pdb.replace('.pdb', '_h.pdb')
+        with open(protonated_pdb, 'w') as f:
+            f.write(Chem.MolToPDBBlock(ligand_mol))
+        
+        print(f"Ligand protonated successfully: {protonated_pdb}")
+        # Load the protonated PDB and replace residues named "UNL" with the residue name
+        lines = []
+        with open(protonated_pdb, 'r') as f:
+            for line in f:
+                if line.startswith("HETATM") or line.startswith("ATOM"):
+                    if line[17:20].strip() == "UNL":
+                        line = line[:17] + f"{residue_name:<3}" + line[20:]
+                lines.append(line)
+        
+        with open(protonated_pdb, 'w') as f:
+            f.writelines(lines)
+        return protonated_pdb
+
+def prepare_ligand(ligand_pdb, protonate=True):
+    """
+    Prepare the ligand using ADFR.
+    """
+    # Protonate the ligand before preparing it
+    if protonate:
+        ligand_pdb = protonate_ligand(ligand_pdb)
+    ligand_pdbqt = ligand_pdb.replace('.pdb', '.pdbqt')
+    try:
+        subprocess.run(['prepare_ligand4', 
+                        '-l', ligand_pdb, 
+                        '-o', ligand_pdbqt], check=True)
+        print(f"Ligand prepared successfully: {ligand_pdbqt}")
+        return ligand_pdbqt
+    except subprocess.CalledProcessError as e:
+        print(f"Error preparing ligand: {e}")
+        raise
+
+def prepare_receptor(receptor_pdb, protonate=True):
+    """
+    Prepare the receptor using ADFR.
+    """
+    if protonate:
+        receptor_pdb = protonate_protein(receptor_pdb)
+    receptor_pdbqt = receptor_pdb.replace('.pdb', '.pdbqt')
+    try:
+        subprocess.run(['prepare_receptor4', 
+                        '-r', receptor_pdb, 
+                        '-o', receptor_pdbqt], check=True)
+        print(f"Receptor prepared successfully: {receptor_pdbqt}")
+        return receptor_pdbqt
+    except subprocess.CalledProcessError as e:
+        print(f"Error preparing receptor: {e}")
+        raise
+
+def calculate_center_of_mass(receptor_pdb, geometry=False):
+    """
+    Calculate the center of mass of the protein using Biopython.
+    The geometry parameter determines whether to use the geometry or the mass of the atoms.
+    """
+    parser = PDBParser(QUIET=True)
+    structure = parser.get_structure('protein', receptor_pdb)
+    atoms = [atom for atom in structure.get_atoms() if atom.element != 'H']
+    
+    total_mass = 0.0
+    weighted_sum = [0.0, 0.0, 0.0]
+    for atom in atoms:
+        mass = 1.0 if geometry else atom.mass
+        total_mass += mass
+        weighted_sum[0] += atom.coord[0] * mass
+        weighted_sum[1] += atom.coord[1] * mass
+        weighted_sum[2] += atom.coord[2] * mass
+    com =  np.array([coord / total_mass for coord in weighted_sum])
+    return com
+
+def estimate_box_size(receptor_pdb):
+    """
+    Estimate the docking box size based on the receptor structure.
+    """
+    parser = PDBParser(QUIET=True)
+    structure = parser.get_structure('protein', receptor_pdb)
+    atoms = [atom for atom in structure.get_atoms() if atom.element != 'H']
+    
+    min_coords = np.min([atom.coord for atom in atoms], axis=0)
+    max_coords = np.max([atom.coord for atom in atoms], axis=0)
+    
+    box_size = max_coords - min_coords
+    return box_size
+
+def run_docking(receptor_pdbqt, 
+                ligand_pdbqt, 
+                output_pdbqt, 
+                center_x, center_y, center_z, 
+                size_x, size_y, size_z, 
+                nprocs=12, gnina=False):
+    """
+    Run the docking using AutoDock Vina or the AI based Gnina.
+    """
+    command_list = [   
+            'vina',
+            '--receptor', receptor_pdbqt, 
+            '--ligand', ligand_pdbqt,
+            '--exhaustiveness', '32',
+            '--num_modes', '1',
+            '--cpu', str(nprocs),
+            '--center_x', str(center_x), '--center_y', str(center_y), '--center_z', str(center_z),
+            '--size_x', str(size_x), '--size_y', str(size_y), '--size_z', str(size_z),    
+            '--out', output_pdbqt
+        ]
+
+    if gnina:
+        command_list[0] = 'gnina' #Replace vina with gnina
+        command_list.extend(['--device','0']) #GPU ID for Gnina
+    try:
+        subprocess.run(command_list, check=True)
+        print(f"Docking completed successfully: {output_pdbqt}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error during docking: {e}")
+        raise
+
+def docking(protein_pdb, ligand_pdb, output_pdbqt, nprocs, verbose=False):
+
+    if verbose:
+        print("Verbose mode enabled.")
+        print("Preparing receptor ...")
+
+    receptor_pdbqt = prepare_receptor(protein_pdb)
+    
+    if verbose:
+        print("Verbose mode enabled.")
+        print("Preparing ligand ...")
+    ligand_pdbqt = prepare_ligand(ligand_pdb)
+
+    if verbose:
+        print("Calculating center of mass and box size ...")
+
+    center_x, center_y, center_z = calculate_center_of_mass(protein_pdb)
+    box_size = estimate_box_size(protein_pdb)
+    size_x, size_y, size_z = box_size
+
+    if verbose:
+        print(f"Center of mass: {center_x}, {center_y}, {center_z}")
+        print(f"Estimated box size: {box_size}")
+
+    if verbose:
+        print("Running docking ...")
+        run_docking(receptor_pdbqt, ligand_pdbqt, output_pdbqt, 
+                center_x, center_y, center_z, 
+                size_x, size_y, size_z, 
+                nprocs=nprocs)
+
+def batch_docking(receptor_pdbqt, ligand_pdbqtdir, 
+                  center_x, center_y, center_z, 
+                  size_x, size_y, size_z,
+                  nprocs, verbose=False):
+    """
+    Perform batch docking for multiple ligands.
+    """
+    receptor_name = os.path.splitext(os.path.basename(receptor_pdbqt))[0]
+    output_dir = os.path.join(os.getcwd(), f'{receptor_name}_docking_results')
+    os.makedirs(output_dir)
+    if verbose:
+        print("Verbose mode enabled.")
+    ligand_pdbqts = [os.path.join(ligand_pdbqtdir, f) for f in os.listdir(ligand_pdbqtdir) if f.endswith('.pdbqt')]
+    command_list = ['gnina', '--receptor', receptor_pdbqt,
+                    '--exhaustiveness', '32', 
+                    '--num_modes', '1', 
+                    '--cpu', str(nprocs),
+                    '--center_x', str(center_x), '--center_y', str(center_y), '--center_z', str(center_z),
+                    '--size_x', str(size_x), '--size_y', str(size_y), '--size_z', str(size_z),'--dir', output_dir]
+    for ligand_pdbqt in ligand_pdbqts:
+        command_list.extend(['--batch', ligand_pdbqt])
+    try:
+        subprocess.run(command_list, check=True)
+        print(f"Docking completed successfully @: {output_dir}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error during docking: {e}")
+        raise
+
+
+def main(nprocs = 12):
+    parser = argparse.ArgumentParser(description='Blind Molecular Docking using AutoDock Vina and AutoLigand.')
+    parser.add_argument('receptor_pdb', type=str, help='Path to the receptor PDB file.')
+    parser.add_argument('ligand_pdb', type=str, help='Path to the ligand PDB file.')
+    parser.add_argument('--output', type=str, default='output.pdbqt', help='Output PDBQT file for the docking results.')
+    parser.add_argument('--verbose', action='store_true', help='Enable verbose output.')
+    args = parser.parse_args()
+
+    
+    args = parser.parse_args()
+    if args.verbose:
+        print("Verbose mode enabled.")
+        print("Preparing receptor ...")
+
+    receptor_pdbqt = prepare_receptor(args.receptor_pdb)
+    
+    if args.verbose:
+        print("Verbose mode enabled.")
+        print("Preparing ligand ...")
+    ligand_pdbqt = prepare_ligand(args.ligand_pdb)
+
+    if args.verbose:
+        print("Calculating center of mass and box size ...")
+
+    center_x, center_y, center_z = calculate_center_of_mass(args.receptor_pdb, geometry=True)
+    box_size = estimate_box_size(args.receptor_pdb)
+    size_x, size_y, size_z = box_size
+
+    if args.verbose:
+        print(f"Center of mass: {center_x}, {center_y}, {center_z}")
+        print(f"Estimated box size: {box_size}")
+
+    if args.verbose:
+        print("Running docking ...")
+    run_docking(receptor_pdbqt, ligand_pdbqt, args.output, 
+                center_x, center_y, center_z, 
+                size_x, size_y, size_z, 
+                nprocs=nprocs, gnina=True)
+
+if __name__ == "__main__":
+    main()
