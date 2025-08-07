97cbc69cdc6726343fc8b7cf0a77cd7c1379564e
hariseldon99 <hariseldon99@gmail.com>
2025-08-08 00:27:27 +0530

    finished preliminary PP of ku04amp02-pea, but noted that genion distorted the protein conformations.

--- Antimicrobial_Peptides/Ku04AMP02_phosphatedylethanolamine/ku04amp02_pea_complex/pea.acpype/rungmx.sh
+++ Antimicrobial_Peptides/Ku04AMP02_phosphatedylethanolamine/ku04amp02_pea_complex/pea.acpype/rungmx.sh
@@ -1,6 +1,7 @@
 
+#!/bin/bash
 echo 0 | gmx editconf -f pea_GMX.gro -bt octahedron -d 1 -c -princ
 gmx grompp -f em.mdp -c out.gro -p pea_GMX.top -o em.tpr -v
-gmx mdrun -ntmpi 1 -v -deffnm em
+gmx mdrun -nt 12 -v -deffnm em
 gmx grompp -f md.mdp -c em.gro -p pea_GMX.top -o md.tpr -r em.gro
-gmx mdrun -ntmpi 1 -v -deffnm md
+gmx mdrun -nt 12 -v -deffnm md
