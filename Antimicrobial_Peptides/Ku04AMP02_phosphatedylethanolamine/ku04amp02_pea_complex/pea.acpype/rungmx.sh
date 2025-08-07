64a34da0a2833da54ea149d08dc2c4645ce7ebb9
hariseldon99 <hariseldon99@gmail.com>
2025-08-07 17:02:07 +0530

    started ku04amp02-pea dynamical sim

--- Antimicrobial_Peptides/Ku04AMP02_phosphatedylethanolamine/ku04amp02_pea_complex/pea.acpype/rungmx.sh
+++ Antimicrobial_Peptides/Ku04AMP02_phosphatedylethanolamine/ku04amp02_pea_complex/pea.acpype/rungmx.sh
@@ -1,5 +1,5 @@
 
-echo 0 | gmx editconf -f pea_GMX.gro -bt octahedron -d 10 -c -princ
+echo 0 | gmx editconf -f pea_GMX.gro -bt octahedron -d 1 -c -princ
 gmx grompp -f em.mdp -c out.gro -p pea_GMX.top -o em.tpr -v
 gmx mdrun -ntmpi 1 -v -deffnm em
 gmx grompp -f md.mdp -c em.gro -p pea_GMX.top -o md.tpr -r em.gro
