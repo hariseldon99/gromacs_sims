59d34e989d04a33099d4f8b89a7f96313f566151
hariseldon99 <hariseldon99@gmail.com>
2026-08-02 13:15:27 +0530

    organized the scripts directory. Remove redundant python files and added membrane builder scripts

---
+++ scripts/python/membrane_builder/memb_build.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+Uses COBY membrane builder to generate a peptide-membrane system with 1 copy of the KU04AMP01 peptide in a lipid bilayer.
+For coarse-grained simulations, the peptide is pre-aligned along the membrane normal (Z-axis) and centered at Z=0.
+"""
+import COBY
+
+sysname = "ku04_gim"
+
+COBY.COBY(
+    box=[14, 14, 8],
+    # Method A: using POPG and DOPE shortcuts to bypass internal diacyl mismatch bugs
+    membrane="lipid:DVPE:46 lipid:POPG:19 lipid:POPE:12 lipid:DOPE:8 lipid:TOCL:2 apl:0.61",
+    protein="file:KU04AMP01_cg_shifted.pdb moleculetypes:KU04 center_protein:False", #Protein was pre-aligned, so do not center again.
+    solvation="default",
+    itp_input="file:top_for_COBY.itp",
+    # Feed COBY the single cardiolipin coordinate file you just generated
+    molecule_import="file:tocl_single.gro moleculetypes:TOCL",
+    out_sys=sysname,
+    out_top=sysname + ".top",
+    out_log=sysname + ".log",
+    sn=sysname,
+)
